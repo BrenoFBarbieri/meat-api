1021eae90d528d2aa9d31af489ec2faf71f14e27
Breno Barbieri <breno.barbieri@kumulus.com.br>
2021-12-13 20:40:09 -0300

    first commit

---
+++ dist/main.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const restify = require("restify");
+const server = restify.createServer({
+    name: 'meat-api',
+    version: '1.0.0'
+});
+server.get('/hello', (req, res, next) => {
+});
---
+++ main.ts
@@ -0,0 +1,10 @@
+import * as restify from 'restify';
+
+const server = restify.createServer({
+    name: 'meat-api',
+    version: '1.0.0'
+});
+
+server.get('/hello', (req, res, next) => {
+    
+});
